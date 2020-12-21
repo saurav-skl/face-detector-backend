75d4a8bc3b4bd8e4c76587aa96d4b0d92535db05
Saurav Lal <sauravkumarlal7060@gmail.com>
2020-12-21 13:47:26 +0530

    adding signin/signup server

--- server.js
+++ server.js
@@ -57,32 +57,34 @@ app.post("/signin", (req, res) => {
 
 app.post("/register", (req, res) => {
   const { email, name, password } = req.body;
-  console.log(password);
+  // console.log(password);
   const hash = bcrypt.hashSync(password, 10);
+  // console.log(email,hash);
   db.transaction((trx) => {
     trx
-      .insert({
-        hash: hash,
-        email: email
-      })
-      .into('login')
-      .returning('email')
-      .then((loginEmail) => {
-        console.log(trx);
-        return trex('person')
-          .returning('*')
+    .insert({
+      hash: hash,
+      email: email,
+    },'id')
+    .into("login")
+    .returning("email")
+    .then((loginEmail) => {
+        // console.log(loginEmail[0]);
+        return trx("person")
+          .returning("*")
           .insert({
             name: name,
             email: loginEmail[0],
             joined: new Date(),
           })
           .then((user) => {
+            console.log(user);
             res.json(user[0]);
           });
       })
       .then(trx.commit)
       .catch(trx.rollback);
-  }).catch((err) => res.status(400).json("unable to register"));
+  }).catch((err) => res.status(400).json(err + "   unable to register"));
 });
 
 app.get("/profile/:id", (req, res) => {
