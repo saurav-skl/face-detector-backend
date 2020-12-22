e639567f512469f475e425f0e564e0628454b8d4
Saurav Lal <sauravkumarlal7060@gmail.com>
2020-12-22 23:26:32 +0530

    Fixing issues

--- server.js
+++ server.js
@@ -3,27 +3,28 @@ const bodyparser = require("body-parser");
 const bcrypt = require("bcrypt");
 var cors = require("cors");
 const knex = require("knex");
+
+
 const register = require("./Control/Register.js");
 const signin = require("./Control/Signin");
 const profile = require("./Control/Profile");
 const image = require("./Control/Image");
 
-
 const db = knex({
   client: "pg",
   connection: {
-   connectionString: process.env.DATABASE_URL,
-   ssl: {
-     rejectUnauthorized:false
-   }
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   },
 });
 
-db.select("*")
-  .from("person")
-  .then((data) => {
-    // console.log(data);
-  });
+// db.select("*")
+//   .from("person")
+//   .then((data) => {
+//     // console.log(data);
+//   });
 
 const app = express();
 
@@ -32,7 +33,7 @@ app.use(cors());
 app.use(bodyparser.json());
 
 app.get("/", (req, res) => {
-  res.send("Its Working");
+  res.send("Its Working" + (db.person));
   //   console.log(database)
 });
 
@@ -52,9 +53,9 @@ app.put("/image", (req, res) => {
   image.handleImage(req, res, db);
 });
 
-app.post("/imageUrl",(req,res)=>{
-  image.handleApi(req,res);
-})
+app.post("/imageUrl", (req, res) => {
+  image.handleApi(req, res);
+});
 
 // app.listen(3001, () => {
 //   console.log("Success on port 3001");
@@ -64,4 +65,5 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 8000;
 }
+console.log('app is running on port '+ port);
 app.listen(port);
